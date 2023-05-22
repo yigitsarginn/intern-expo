f1435e537088f912e348a6e42f35c2a6a78efbc3
Emre SÖYLEMEZ <48524826+EmreSoylemz@users.noreply.github.com>
2023-05-22 18:16:09 +0300

    feat: CustomDivider added

---
+++ src/components/CustomDivider/CustomDivider.style.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  divider: {
+    marginVertical: 10,
+    width: "91%",
+    borderBottomColor: "grey",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+});
